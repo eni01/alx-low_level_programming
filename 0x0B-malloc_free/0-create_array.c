9e0bb0c1c5ab4fe035dfef58d2759d34a03163d7
eni01 <oreolaniyan01@gmail.com>
2022-05-05 15:06:39 -0700

    mycommit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,7 +6,7 @@
  * and initializes it with a specific char
  * @size: size of the array to create
  * @c: char to initialize the array c
- * 
+ *
  * Return: pointer to the array (Success), NULL (Error)
  */
 char *create_array(unsigned int size, char c)
@@ -29,6 +29,6 @@ char *create_array(unsigned int size, char c)
 	}
 
 	*(p + i) = '\0';
-	 
+ 
 	return (p);
 }
